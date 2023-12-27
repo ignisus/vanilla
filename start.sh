debdc04f7c150f2f093d02c9a2d5855135a551cf
Daniel Delgado Jiménez <61506276+eldiosx@users.noreply.github.com>
2023-12-27 18:54:47 +0100

    minorUpdate

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #Survival
 cd /mnt/ignisus/ignisusland/survival
-java -Xmx10192M -Xms10192M -jar --add-modules=jdk.incubator.vector purpur*.jar nogui
+java -Xmx12192M -Xms12192M -jar --add-modules=jdk.incubator.vector purpur*.jar nogui
